6b9bfdcabe8dd7db3340195a7a0f5c0590845eea
Ravitank7046 <ravi.t@bitcodingsolutions.com>
2026-02-02 19:00:01 +0530

    02/02/2026 C

--- api/myapp/views.py
+++ api/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib import contenttypes
 from django.template.context_processors import request
 from .models import Student
@@ -9,12 +9,9 @@ from .serializers import StudentSerializer
 def student_details(request,pk):
     stu = Student.objects.get(id=pk)
     ser = StudentSerializer(stu)
-    json_data = JSONRenderer().render(ser.data)
-    return HttpResponse(json_data,content_type='application/json')
-
+    return JsonResponse(ser.data)
 
 def student_Alldetails(request):
     stu = Student.objects.all()
     ser = StudentSerializer(stu,many=True)
-    json = JSONRenderer().render(ser.data)
-    return HttpResponse(json,content_type='application/json') 
+    return JsonResponse(ser.data) 
