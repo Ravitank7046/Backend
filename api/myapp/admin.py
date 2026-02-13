9b66c1bde383a276feae09d48e25002a9b510672
Ravitank7046 <ravi.t@bitcodingsolutions.com>
2026-02-13 10:45:31 +0530

    push before crud api

--- api/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from myapp.models import Student
-# Register your models here.
-@admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ['id','roll','name','city']
-    
