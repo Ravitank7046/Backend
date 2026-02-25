938a5711cdcc97ba3c83e127815d75e350fec074
rvtankbitcoding <ravi.t@bitcodingsolutions.com>
2026-02-25 21:09:38 +0530

    25/02 H

--- JWTAuth/JWTAuth/wsgi.py
+++ JWTAuth/JWTAuth/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TokenBaseAuth project.
+WSGI config for JWTAuth project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TokenBaseAuth.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'JWTAuth.settings')
 
 application = get_wsgi_application()
--- TokenBaseAuth/api/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Student(models.Model):
-  name = models.CharField(max_length=100)
-  city = models.CharField(max_length=100)
-  aadharNumber = models.IntegerField()
-  schoolName = models.CharField(max_length=100)
-  
